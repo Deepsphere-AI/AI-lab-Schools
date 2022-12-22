24093467a6c4e852f5889eb2b8acb75ac94b9417
danishdeepsphere <120715128+danishdeepsphere@users.noreply.github.com>
2022-12-22 10:42:08 +0530

    Update app.py

--- Streamlitapp/Grade-07/app.py
+++ Streamlitapp/Grade-07/app.py
@@ -21,7 +21,7 @@ col1, col2, col3 = vAR_st.columns((2,5,3))
 with col1:
     vAR_st.write("")
 with col2:
-    vAR_st.image('https://github.com/danishdeepsphere/AI_Schools/blob/main/image/Logo_final.png')
+    vAR_st.image('image/Logo_final.png')
 with col3:
     vAR_st.write("")
 
