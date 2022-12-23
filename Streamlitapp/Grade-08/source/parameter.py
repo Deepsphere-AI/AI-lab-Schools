dfd6cca56aaa1b594bab48157d54c0387385954d
VikramanDeepsphere <121026513+VikramanDeepsphere@users.noreply.github.com>
2022-12-23 13:40:58 +0530

    Update parameter.py

--- Streamlitapp/Grade-08/source/parameter.py
+++ Streamlitapp/Grade-08/source/parameter.py
@@ -9,29 +9,29 @@ from PIL import Image
 import source.title_1 as head
 def area_perameter():
     head.title()
-    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to Find the Perimeter </p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to Find the Parimeter from Area </p>", unsafe_allow_html=True)
     st.markdown("<hr style=height:2.5px;background-color:gray>",unsafe_allow_html=True)
     w1,col1,col2,w2=st.columns((1,2,2,1))
     us1,bc1,bc2,us2=st.columns((4,2.5,2.8,6))
     with col1:
         st.markdown("")
-        st.write("# Enter the Area of a Square ")
+        st.write("# Side of a Square Perimeter ")
     # ------------to create the function to clear the input-----------#
     with bc2:
         st.markdown("")
         st.markdown("")
         def clear_text():
-            st.session_state["Clear_parameter"] = ""
+            st.session_state["Clear_parameter"] = 0
         st.button("Clear", on_click=clear_text)   
     with col2:
-        vAR_input_num=st.text_input("",placeholder="In CM",key="Clear_parameter")  
+        vAR_input_num=st.number_input("",min_value=0.00,step=1.0,key="Clear_parameter")  
     #----- Date to Day -------#
     with bc1:
         st.markdown("")
         st.markdown("")
         if st.button("Submit"):
             with col2:
-                if  vAR_input_num !='':
+                if  vAR_input_num == int or float:
                     vAR_input_num=int(vAR_input_num)
                     vAR_parameter=4 * vAR_input_num
                     st.markdown("### ")
