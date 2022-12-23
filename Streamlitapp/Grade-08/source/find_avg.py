d98cb045fe352fe783d8e62affa99b0eb3fa688c
VikramanDeepsphere <121026513+VikramanDeepsphere@users.noreply.github.com>
2022-12-23 13:46:40 +0530

    Update find_avg.py

--- Streamlitapp/Grade-08/source/find_avg.py
+++ Streamlitapp/Grade-08/source/find_avg.py
@@ -10,13 +10,13 @@ import source.title_1 as head
 #         st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 def average():
     head.title()
-    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to Find the Average </p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to find the Average </p>", unsafe_allow_html=True)
     st.markdown("<hr style=height:2.5px;background-color:gray>",unsafe_allow_html=True)
     w1,col1,col2,w2=st.columns((1,2,2,1))
     us1,bc1,bc2,us2=st.columns((4,2.5,2.8,6))
     with col1:
         st.markdown("")
-        st.write("# Enter the Date ")
+        st.write("# Enter the Number ")
     # ------------to create the function to clear the input-----------#
     with bc2:
         st.markdown("")
@@ -25,7 +25,7 @@ def average():
             st.session_state["Clear_Avg"] = ""
         st.button("Clear", on_click=clear_text)   
     with col2:
-        vAR_input_num=st.text_input("",key="Clear_Avg")  
+        vAR_input_num=st.text_input("",placeholder="Eg:00,00,00",key="Clear_Avg")  
         vAR_list=[]
     #----- Average -------#
     with bc1:
@@ -46,4 +46,4 @@ def average():
                 else:
                     st.error("Error")
                 with col1:
-                     st.write("# Answer is ")
+                     st.write("# Average ")
