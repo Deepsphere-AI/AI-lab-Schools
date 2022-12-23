cee7cb594ab37e068c50742ebab7108ec53794bc
VikramanDeepsphere <121026513+VikramanDeepsphere@users.noreply.github.com>
2022-12-23 13:16:11 +0530

    Update find_cube.py

--- Streamlitapp/Grade-08/source/find_cube.py
+++ Streamlitapp/Grade-08/source/find_cube.py
@@ -8,7 +8,7 @@ import source.title_1 as head
 #         st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 def cube():
     head.title()
-    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to Find the Cube </p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to find the Cube </p>", unsafe_allow_html=True)
     st.markdown("<hr style=height:2.5px;background-color:gray>",unsafe_allow_html=True)
     w1,col1,col2,w2=st.columns((1,2,2,1))
     us1,bc1,bc2,us2=st.columns((4,2.5,2.8,6))
@@ -20,17 +20,17 @@ def cube():
         st.markdown("")
         st.markdown("")
         def clear_text():
-            st.session_state["Clear_Cube"] = ""
+            st.session_state["Clear_Cube"] = 0
         st.button("Clear", on_click=clear_text)   
     with col2:
-        vAR_input_num=st.text_input("",key="Clear_Cube")  
+        vAR_input_num=st.number_input("",min_value=0.00,step=1.0,key="Clear_Cube")  
     #-----cube-------#
     with bc1:
         st.markdown("")
         st.markdown("")
         if st.button("Submit"):
             with col2:
-                if vAR_input_num.isnumeric():
+                if vAR_input_num == int or float:
                     vAR_input_num=int(vAR_input_num)
                     cube=lambda vAR_x: vAR_x**3
                     cube=cube(vAR_input_num)
@@ -38,4 +38,4 @@ def cube():
                 else:
                     st.error("Error")
                 with col1:
-                    st.write("# Answer is ")
+                    st.write("# Cube ")
