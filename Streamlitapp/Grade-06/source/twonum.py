8198fe9a073a2a4885a621d9d3ccd26dfc96a594
Santhoshdeepsphere <120715127+Santhoshdeepsphere@users.noreply.github.com>
2022-12-24 22:55:42 +0530

    Update twonum.py

--- Streamlitapp/Grade-06/source/twonum.py
+++ Streamlitapp/Grade-06/source/twonum.py
@@ -5,37 +5,38 @@ def comparision():
         if vAR_st.button("Submit"):
             with col2:
                 vAR_st.write("")
-                vAR_st.markdown("### Answer is")
+                vAR_st.write('')
+                vAR_st.subheader("Result")
             if vAR_num1>vAR_num2:
                 with col3:
                     vAR_st.write("")
-                    vAR_st.subheader("A is greater than B")
+                    vAR_st.success("A is greater than B")
             elif vAR_num1<vAR_num2:
                 with col3:
                     vAR_st.write("")
-                    vAR_st.subheader("B is greater than A")
+                    vAR_st.success("B is greater than A")
             else:
                 with col3:
                     vAR_st.write('')
-                    vAR_st.subheader("Both the numbers are equal")
+                    vAR_st.success("Both the numbers are equal")
 
 
-    col1,col2,col3,col4=vAR_st.columns((1.7,2,2.4,1.5))
+    col1,col2,col3,col4=vAR_st.columns((1,2,2,1))
     with col2:
         vAR_st.write("")
         vAR_st.write("")
-        vAR_st.markdown("### Enter A")
+        vAR_st.markdown("### Enter number 1")
     with col3:
         vAR_num1=vAR_st.number_input("",key="Clear")
     with col2:
         vAR_st.write("")
         vAR_st.write("")
-        vAR_st.markdown("### Enter B")
+        vAR_st.markdown("### Enter number 2")
     with col3:
         vAR_num2=vAR_st.number_input("",key="Clear2")
     
 
-    col5,col6,col7,col8=vAR_st.columns((4,1.4,1.4,4))
+    col5,col6,col7,col8=vAR_st.columns((5,2,2,5))
     with col6:
         vAR_st.write('')
         comp()
