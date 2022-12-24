85a3d7b2469d5772ae6ad838c31d67dacfc91301
Santhoshdeepsphere <120715127+Santhoshdeepsphere@users.noreply.github.com>
2022-12-24 22:54:38 +0530

    Update tables.py

--- Streamlitapp/Grade-06/source/tables.py
+++ Streamlitapp/Grade-06/source/tables.py
@@ -1,42 +1,43 @@
-import streamlit as st
+import streamlit as vAR_st
 from streamlit_option_menu import option_menu
 
 
 def tables():
-    w1,col1,col2,w2=st.columns((1,2,2,1))
-    us1,bc1,bc2,us2=st.columns((5,2,2,5))
+    w1,col1,col2,w2=vAR_st.columns((1,2,2,1))
+    us1,bc1,bc2,us2=vAR_st.columns((5,2,2,5))
     with col1:
-        st.write("")
-        st.write("")
-        st.subheader("Enter the Number ")
+        vAR_st.write("")
+        vAR_st.write("")
+        vAR_st.write('')
+        vAR_st.subheader("Enter the Number ")
   
     with bc2:
-        st.markdown("")
-        st.markdown("")
+        vAR_st.markdown("")
+        vAR_st.markdown("")
         def clear_text():
-                st.session_state["text"] = ""
-        st.button("Clear", on_click=clear_text) 
+                vAR_st.session_state["text"] = ""
+        vAR_st.button("Clear", on_click=clear_text) 
     # ---------input button----------#
     with col2:
-        st.markdown("")
-        vAR_input_number=st.text_input("",key="text",placeholder= "Eg: 3")
+        vAR_st.markdown("")
+        vAR_input_number=vAR_st.text_input("",key="text",placeholder= "Eg: 3")
     with bc1:
-        st.markdown("")
-        st.markdown("")
-        if st.button("Submit"):
+        vAR_st.markdown("")
+        vAR_st.markdown("")
+        if vAR_st.button("Submit"):
             with col2:
-                st.markdown("")
+                vAR_st.markdown("")
                 if vAR_input_number.isnumeric():
                     vAR_num=int(vAR_input_number)
                     for i in range(1,11):
-                        x=vAR_num, 'x', i, "=" ,vAR_num*i
-                        x=','.join(str(item) for item in x)
-                        y=x.replace(",","")  
-                        st.subheader(y)
+                        vAR_x=vAR_num, 'x', i, "=" ,vAR_num*i
+                        vAR_x=','.join(str(item) for item in vAR_x)
+                        vAR_y=vAR_x.replace(",","")  
+                        vAR_st.subheader(vAR_y)
                     with col1:
-                        st.subheader("ㅤㅤㅤTables are ")
+                        vAR_st.subheader("ㅤㅤㅤTables")
                 elif vAR_input_number==str(vAR_input_number):
-                    st.subheader("Enter a number")
+                    vAR_st.subheader("Enter a number")
                 else:
-                    st.subheader("Enter a number")
+                    vAR_st.subheader("Enter a number")
                 
