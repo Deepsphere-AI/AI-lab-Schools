067c9280aed0f75e6a349ed4fb8b1379564c729b
VikramanDeepsphere <121026513+VikramanDeepsphere@users.noreply.github.com>
2022-12-27 17:40:37 +0530

    Update union.py

--- Streamlitapp/Grade-09/source/union.py
+++ Streamlitapp/Grade-09/source/union.py
@@ -56,6 +56,7 @@ def intersec(a,b):
 
 def uni_int():
     head.title()
+    vAR_st.markdown("<hr style=height:2.5px;background-color:gray>",unsafe_allow_html=True)
     w1,col1,col2,w2=vAR_st.columns((1,2,2,1))
     us1,bc1,bc2,us2=vAR_st.columns((4,1.4,1.4,4))
         
