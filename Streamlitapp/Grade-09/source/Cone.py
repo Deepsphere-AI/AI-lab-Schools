8a9e03d6b3e5b886d040dd8b68843ff3be8f5f91
VikramanDeepsphere <121026513+VikramanDeepsphere@users.noreply.github.com>
2022-12-28 17:19:58 +0530

    Update Cone.py

--- Streamlitapp/Grade-09/source/Cone.py
+++ Streamlitapp/Grade-09/source/Cone.py
@@ -22,7 +22,7 @@ def cone_1():
         def clear_text():
             st.session_state["Clear_cone_Radius"] = 0
             st.session_state["Clear_cone_Height"] = 0
-            st.session_state["text1_selectbox"] = "Select"
+            st.session_state["text1_selectbox"] = "Surface Area"
         st.button("Clear", on_click=clear_text)   
     with col2:
         vAR_input_Radius=st.number_input("",min_value=0.00,step=1.0,key="Clear_cone_Radius") 
