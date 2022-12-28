87a9ea41dcb19ac813c4bd11d57b94e2cfa5322f
VikramanDeepsphere <121026513+VikramanDeepsphere@users.noreply.github.com>
2022-12-28 16:48:38 +0530

    Update Cube.py

--- Streamlitapp/Grade-09/source/Cube.py
+++ Streamlitapp/Grade-09/source/Cube.py
@@ -4,7 +4,7 @@ import math
 import source.title_1 as head
 def cube_1():
     head.title()
-    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to find the Cube </p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to find the Surface Area and Volume of the Cube </p>", unsafe_allow_html=True)
     st.markdown("<hr style=height:2.5px;background-color:gray>",unsafe_allow_html=True)
     w1,col1,col2,w2=st.columns((1,2,2,1))
     us1,bc1,bc2,us2=st.columns((4,3,3,6))
