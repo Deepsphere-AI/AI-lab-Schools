a7725a1589540f2cf2f92df62cfc5dc2e86110ea
VikramanDeepsphere <121026513+VikramanDeepsphere@users.noreply.github.com>
2022-12-30 10:38:53 +0530

    Update section_formula.py

--- Streamlitapp/Grade-10/source/section_formula.py
+++ Streamlitapp/Grade-10/source/section_formula.py
@@ -4,7 +4,7 @@ import math
 import source.title_1 as head
 def Section_1():
     head.title()
-    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to Section Formula </p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to Section Formuls </p>", unsafe_allow_html=True)
     st.markdown("<hr style=height:2.5px;background-color:gray>",unsafe_allow_html=True)
     w1,col1,col2,w2=st.columns((1,2,2,1))
     us1,bc1,bc2,us2=st.columns((4,3,3,6))
@@ -32,15 +32,21 @@ def Section_1():
         vAR_input_x2=st.number_input("",min_value=0.00,step=1.0,key="Clear_sec_x2")
         vAR_input_m1=st.number_input("",min_value=0.00,step=1.0,key="Clear_sec_m1")
         vAR_input_m2=st.number_input("",min_value=0.00,step=1.0,key="Clear_sec_m2") 
+        selected=st.selectbox("",["Internal Section","External Section"])
     #-----cylinder-------#
     with bc1:
         st.markdown("")
         st.markdown("")
         if st.button("Submit"):
             with col2:
-                if vAR_input_x1 and vAR_input_x2 and vAR_input_m1 and vAR_input_m2!= 0:
-                    vAR_dis = (((vAR_input_m1*vAR_input_x2) + (vAR_input_m2*vAR_input_x1))/(vAR_input_m1 + vAR_input_m2))
-                    st.success(vAR_dis)
+                
+                if vAR_input_x1 and vAR_input_x2 and vAR_input_m1 and vAR_input_m2 != 0:
+                    if selected == "Internal Section":
+                        vAR_dis = (((vAR_input_m1*vAR_input_x2) + (vAR_input_m2*vAR_input_x1))/(vAR_input_m1 + vAR_input_m2))
+                        st.success(vAR_dis)
+                    if selected == "External Section":
+                        vAR_dis = (((vAR_input_m1*vAR_input_x2) - (vAR_input_m2*vAR_input_x1))/(vAR_input_m1 - vAR_input_m2))
+                        st.success(vAR_dis)
                 else:
                     st.error("Error")
                 with col1:
