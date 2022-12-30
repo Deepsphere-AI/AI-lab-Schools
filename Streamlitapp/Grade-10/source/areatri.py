7aecadbc889fd72b9b0ee351621acb412aa65625
VikramanDeepsphere <121026513+VikramanDeepsphere@users.noreply.github.com>
2022-12-30 11:52:56 +0530

    Update areatri.py

--- Streamlitapp/Grade-10/source/areatri.py
+++ Streamlitapp/Grade-10/source/areatri.py
@@ -1,27 +1,21 @@
 import streamlit as vAR_st
 import streamlit as vAR_st
-#from streamlit_option_menu import option_menu
-#import math
-#import source.title_1 as head
+from streamlit_option_menu import option_menu
+import math
+import source.title_1 as head
 def atri():
-    #head.title()
+    head.title()
     vAR_st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to find the area of triangle </p>", unsafe_allow_html=True)
     vAR_st.markdown("<hr style=height:2.5px;background-color:gray>",unsafe_allow_html=True)
-    w1,col1,col2,w2=vAR_st.columns((1,2,2,1))
+    w1,col1,col2,col3,w2=vAR_st.columns((1,3,2,2,1))
     us1,bc1,bc2,us2=vAR_st.columns((4,2.5,2.8,6))
     with col1:
-        vAR_st.markdown("")
-        vAR_st.write("# Enter x1 ")
         vAR_st.markdown("### ")
-        vAR_st.write("# Enter y1 ")
-        vAR_st.markdown("### ")
-        vAR_st.write("# Enter x2 ")
-        vAR_st.markdown("### ")
-        vAR_st.write("# Enter y2 ")
-        vAR_st.markdown("### ")
-        vAR_st.write("# Enter x3 ")
-        vAR_st.markdown("### ")
-        vAR_st.write("# Enter y3 ")
+        vAR_st.subheader("Enter x1 and y1")
+        vAR_st.markdown("# ")
+        vAR_st.subheader("Enter x2 nd y2 ")
+        vAR_st.markdown("# ")
+        vAR_st.subheader("Enter x3 and y3")
     # ------------to create the function to clear the input-----------#
     with bc2:
         vAR_st.markdown("")
@@ -36,10 +30,15 @@ def atri():
         vAR_st.button("Clear", on_click=clear_text)   
     with col2:
         vAR_input_x1=vAR_st.number_input("",key="Clear_x1") 
+    with col3:
         vAR_input_y1=vAR_st.number_input("",key="Clear_y1")
+    with col2:   
         vAR_input_x2=vAR_st.number_input("",key="Clear_x2")
+    with col3:
         vAR_input_y2=vAR_st.number_input("",key="Clear_y2")
+    with col2:    
         vAR_input_x3=vAR_st.number_input("",key="Clear_x3")
+    with col3:
         vAR_input_y3=vAR_st.number_input("",key="Clear_y3")
         
     #--------------------------#
@@ -49,8 +48,9 @@ def atri():
         if vAR_st.button("Submit"):
             with col2:
                     vAR_res=0.5*((vAR_input_x1*(vAR_input_y2-vAR_input_y3)+vAR_input_x2*(vAR_input_y3-vAR_input_y1)+vAR_input_x3*(vAR_input_y1-vAR_input_y2)))
-                    vAR_st.subheader((vAR_res)) 
-                    vAR_st.subheader("Sq units")
+                    vAR_st.success((str(vAR_res)+ " Sq units")) 
+
             
-        with col1:
-            vAR_st.write("# Result ")
+            with col1:
+                vAR_st.write("# Result ")
+
